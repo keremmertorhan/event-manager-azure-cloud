a108311b56cd4f776efb1153aba27df9f1e10480
keremmertorhan <kerem.mertorhan17@gmail.com>
2026-02-18 15:27:33 +0100

    ı changed settings.py file

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -1,24 +1,28 @@
 import os
 from pathlib import Path
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-fallback-key-123')
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 ALLOWED_HOSTS = ['*']
 
-
+# CSRF Ayarları 
 CSRF_TRUSTED_ORIGINS = [
     'https://kerem-event-manager-dzfpegdwbqdsbzea.polandcentral-01.azurewebsites.net',
     'https://*.azurewebsites.net'
 ]
 
+# Application definition
 INSTALLED_APPS = [
     'events',
     'users',
-    'storages',
+    'storages',  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,7 +60,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'event_manager.wsgi.application'
 
-
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -70,19 +75,30 @@ DATABASES = {
 
 
 
-AZURE_CONNECTION_STRING = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
-if AZURE_CONNECTION_STRING:
-    DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-    AZURE_CONTAINER = 'media'
-    
-    
-    AZURE_ACCOUNT_NAME = 'keremstorage01' 
-    
-    MEDIA_URL = f'https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER}/'
+DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+
+
+AZURE_ACCOUNT_NAME = 'keremstorage01'
+AZURE_CONTAINER = 'media'
+
+
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
+
+
+AZURE_OVERWRITE_FILES = True
+
+
+MEDIA_URL = f'https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER}/'
+
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login Redirects
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
